b3fb313a1c02c75e51fc282a676e5f81e5045834
jailanhammad <jailanhammad@gmail.com>
2026-04-03 05:14:03 +0200

    home-page-updates

--- src/components/carsection.jsx
+++ src/components/carsection.jsx
@@ -1,44 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './carsection.css';
 import car1 from "../assets/home/car1.svg";
 import car2 from "../assets/home/car2.svg";
 import up from "../assets/home/up.svg";
 
 const Cars = () => {
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('hms-active');
+                }
+            });
+        }, { threshold: 0.2 }); // Trigger when 20% of the card is visible
+
+        const cards = document.querySelectorAll('.hms-reveal-left, .hms-reveal-right');
+        cards.forEach(card => observer.observe(card));
+
+        return () => observer.disconnect();
+    }, []);
+
     return (
         <>
     
 
 
+
 <section className="cta-section">
-    <div className="container-2">
-        <div className="cta-card">
-            <img src={car1} className='car-img' alt="Buy a car" />
-            <div className="cta-overlay">
-                <h3>Are You Looking <br/> For a Car?</h3>
-                <p>We are committed to providing our customers with exceptional service.</p>
-                <button className="cta-btn">
-                    Get Started <i className="fas fa-arrow-right"></i>
-                    <img src={up}  className='up-icon'/>
-                </button>
-            </div>
-        </div>
+            <div className="container-2">
+                
+                <div className="cta-card hms-reveal-left">
+                    <img src={car1} className='car-img' alt="Buy" />
+                    <div className="cta-overlay">
+                        <h3>Are You Looking <br/> For a Car?</h3>
+                        <p>We are committed to providing our customers with exceptional service.</p>
+                        <button className="cta-btn">
+                            Get Started <img src={up} className='up-icon' alt="up" />
+                        </button>
+                    </div>
+                </div>
 
-        <div className="cta-card">
-            <img src={car2} className='car-img' alt="Sell a car" />
-            <div className="cta-overlay">
-                <h3 className='h3'>Do You Want to <br/> Sell a Car?</h3>
-                <p>We are committed to providing our customers with exceptional service.</p>
-                <button className="cta-btn">
-                    Get Started <i className="fas fa-arrow-right"></i>
-                    <img src={up}  className='up-icon'/>
-                </button>
+                <div className="cta-card card-right hms-reveal-right">
+                    <img src={car2} className='car-img' alt="Sell" />
+                    <div className="cta-overlay">
+                        <h3 className='h3'>Do You Want to <br/> Sell a Car?</h3>
+                        <p>We are committed to providing our customers with exceptional service.</p>
+                        <button className="cta-btn">
+                            Get Started <img src={up} className='up-icon' alt="up" />
+                        </button>
+                    </div>
+                </div>
+
             </div>
-        </div>
-    </div>
 </section>
-
-
 
         </>
       );
